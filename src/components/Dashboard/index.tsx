eb27e20fa902f93c772012f54d05801ea2bea026
vitor <santos_vitao@hotmail.com>
2021-10-13 01:36:35 -0300

    small fix

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -2,17 +2,6 @@ import { Summary } from '../Summary';
 import { TransactionsTable } from '../TransactionsTable';
 import { Container } from './styles';
 
-interface TransactionsProps {
-  data: {
-    id: number;
-    title: string;
-    amount: number;
-    type: string;
-    category: string;
-    createdAt: Date;
-  }
-}
-
 export function Dashboard(props: any) {
   return (
     <Container>
